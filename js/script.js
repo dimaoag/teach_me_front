fe19e9b23f85c3902536e9974275fea6128f5639
Dmytro Orikhovskyi <dimaoag@gmail.com>
2018-09-24 17:54:19 +0300

    order-popup

--- js/script.js
+++ js/script.js
@@ -371,3 +371,8 @@ $('.orders-slider').slick({
     slidesToShow: 1,
     adaptiveHeight: true
 });
+
+$('.open-order-popup').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
